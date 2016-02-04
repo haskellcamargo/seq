79ef0ec8ca4d603376abb5df0639e83c80d52578
Marcelo Camargo <marcelocamargo@linuxmail.org>
2016-02-04 14:16:27 -0200

    Reverted shit commit

--- src/Seq.java
+++ src/Seq.java
@@ -27,7 +27,7 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
   /**
    * Applies a function to each item in the list and returns the original list.
    * Used for side effects.
-   * @param fn Action<T>
+   * @param fn
    * @return Seq<T>
    */
   public Seq<T> each(Action<T> fn) {
@@ -40,7 +40,7 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
   /**
    * Applies a function to each item in the list, and produces a new list with
    * the results. The length of the result is the same length as the input.
-   * @param fn Function<Ret, T>
+   * @param fn
    * @return Seq<T>
    */
   public <Ret> Seq<Ret> map(Function<Ret, T> fn) {
@@ -69,7 +69,7 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
   /**
    * Returns a new list composed of the items which pass the supplied
    * function's test.
-   * @param fn Function<Boolean, T>
+   * @param fn
    * @return Seq<T>
    */
   public Seq<T> filter(Function<Boolean, T> fn) {
@@ -85,7 +85,7 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
   /**
    * Like filter, but the new list is composed of the items which fail the
    * function's test
-   * @param fn Function<Boolean, T>
+   * @param fn
    * @return Seq<T>
    */
   public Seq<T> reject(Function<Boolean, T> fn) {
@@ -101,7 +101,7 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
   /**
    * Equivalent to `{ xs.filter(f), xs.reject(f) }`, but more efficient,
    * using only one loop.
-   * @param fn Function<Boolean, T>
+   * @param fn
    * @return Seq<Seq<T>>
    */
   public Seq<Seq<T>> partition(Function<Boolean, T> fn) {
@@ -126,7 +126,7 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
   /**
    * Returns the first item in list to pass the function's test. Returns
    * null if all items fail the test.
-   * @param fn Function<Boolean, T>
+   * @param fn
    * @return T
    */
   public T find(Function<Boolean, T> fn) {
@@ -163,16 +163,27 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
     return size > 0 ? this.list.get(size - 1) : null;
   }
 
-  @SuppressWarnings("unchecked")
+  /**
+   * Everything but the last item of the list.
+   * @return Seq<T>
+   */
   public Seq<T> initial() {
     int size = this.list.size();
-    return (Seq<T>) (size > 0 ? new Seq<>(this.list.subList(0, size)) : new Seq<>());
+    return (Seq<T>) (size > 0 ? new Seq<T>(this.list.subList(0, size)) : new Seq<T>());
   }
 
+  /**
+   * Whether the list is empty.
+   * @return boolean
+   */
   public boolean empty() {
     return this.list.size() == 0;
   }
 
+  /**
+   * Returns a new list which is the reverse of the inputted one.
+   * @return Seq<T>
+   */
   public Seq<T> reverse() {
     Seq<T> buffer = new Seq<T>();
     for (int i = this.list.size() - 1; i >= 0; i--) {
@@ -181,6 +192,10 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
     return buffer;
   }
 
+  /**
+   * Returns a new list which contains each value of the inputted list only once.
+   * @return Seq<T>
+   */
   public Seq<T> unique() {
     Seq<T> buffer = new Seq<T>();
     for (T item : this.list) {
@@ -191,6 +206,13 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
     return buffer;
   }
 
+  /**
+   * Returns a new list which contains each item which has a unique value when
+   * applied to the supplied function. If there are multiple different items
+   * with the same value when the function is applied, the first item is taken.
+   * @param fn
+   * @return Seq<T>
+   */
   public <Ret> Seq<T> uniqueBy(Function<Ret, T> fn) {
     Seq<T> buffer = new Seq<T>();
     Seq<Ret> seen = new Seq<Ret>();
@@ -206,6 +228,14 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
     return buffer;
   }
 
+  /**
+   * Takes a list of items, and using the binary function supplied, folds them
+   * into a single value. Requires an initial value (the second argument),
+   * which will be the starting point, and result in case of an empty list.
+   * @param fn
+   * @param memo
+   * @return T
+   */
   public T fold(BinaryFunction<T, T, T> fn, T memo) {
     for (T item : this.list) {
       memo = fn.call(memo, item);
@@ -213,18 +243,41 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
     return memo;
   }
 
+  /**
+   * Alias to fold.
+   * @param fn
+   * @param memo
+   * @return T
+   */
   public T foldl(BinaryFunction<T, T, T> fn, T memo) {
     return this.fold(fn, memo);
   }
 
+  /**
+   * Like fold, except assumes a non-empty list, and thus doesn't require an
+   * initial value.
+   * @param fn
+   * @return T
+   */
   public T fold1(BinaryFunction<T, T, T> fn) {
-    return this.tail().fold(fn, this.list.get(0));
+    return this.tail().fold(fn, this.head());
   }
 
+  /**
+   * Alias to fold1
+   * @param fn BinaryFunction<T, T, T>
+   * @return T
+   */
   public T foldl1(BinaryFunction<T, T, T> fn) {
-    return this.foldl(fn, this.head());
+    return this.fold1(fn);
   }
 
+  /**
+   * Like fold, except folding from the right instead of the left.
+   * @param fn
+   * @param memo
+   * @return T
+   */
   public T foldr(BinaryFunction<T, T, T> fn, T memo) {
     for (T item : this.reverse().toArrayList()) {
       memo = fn.call(item, memo);
@@ -232,6 +285,11 @@ public class Seq<T> implements Comparable<Seq<T>>, Iterable<T> {
     return memo;
   }
 
+  /**
+   * Like foldr, except assumes a non-empty list, and thus doesn't require an initial value.
+   * @param fn
+   * @return
+   */
   public T foldr1(BinaryFunction<T, T, T> fn) {
     return this.initial().foldr(fn, this.last());
   }
